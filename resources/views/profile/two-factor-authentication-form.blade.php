2d42a79142fbf2dfd2195b84e588584e67f09e84
alila3883 <alila3883@gmail.com>
2020-11-03 20:21:33 +0300

    first commit

---
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -0,0 +1,53 @@
+<div class="container pt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Two Factor Authentication') }}</div>
+
+                <div class="card-body">
+                    @if (!auth()->user()->two_factor_secret)
+                        <div class="text-muted m-3">
+                            <h5>{{ __('You have not enabled two factor authentication.') }}</h5>
+                            {{ __('When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone\'s Google Authenticator application.') }}
+                        </div>
+
+                        <form method="POST" action="{{ url('user/two-factor-authentication') }}">
+                            @csrf
+                            <button type="submit" class="btn btn-primary btn-block">{{ __('Enable Two-Factor') }}</button>
+                        </form>
+                    @else
+                        <div class="text-muted m-3">
+                            <h5>{{ __('You have enabled two factor authentication.') }}</h5>
+                            <p>{{ __('When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone\'s Google Authenticator application.') }}</p>
+                            <p>{{ __('Two factor authentication is now enabled. Scan the following QR code using your phone\'s authenticator application.') }}</p>
+                        </div>
+
+                        <div class="border border-primary rounded text-center mb-3 p-3">
+                            {!! auth()->user()->twoFactorQrCodeSvg() !!}
+                        </div>
+
+                        <div class="text-muted m-3">
+                            <p>{{ __('Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.') }}</p>
+                        </div>
+
+                        <div class="border border-primary rounded text-center mb-3 p-3">
+                            @foreach (json_decode(decrypt(auth()->user()->two_factor_recovery_codes), true) as $code)
+                                <div class="text-muted">{{ $code }}</div>
+                            @endforeach
+                        </div>
+
+                        <a href="javascript:void(0);" class="btn btn-primary" onclick="document.getElementById('regenerate_two_factor_auth').submit();">{{ __('Regenerate Recovery Codes') }}</a>
+                        <a href="javascript:void(0);" class="btn btn-danger" onclick="document.getElementById('delete_two_factor_auth').submit();">{{ __('Disable Two-Factor') }}</a>
+
+                        <form action="{{ url('user/two-factor-recovery-codes') }}" method="POST" id="regenerate_two_factor_auth" style="display: none">@csrf</form>
+                        <form action="{{ url('user/two-factor-authentication') }}" method="POST" id="delete_two_factor_auth" style="display: none">
+                            @csrf
+                            @method('DELETE')
+                        </form>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
---
+++ resources/views/profile/update-password-form.blade.php
@@ -0,0 +1,69 @@
+<div class="container pt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Update password') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('user-password.update') }}">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-group row">
+                            <label for="current_password" class="col-md-4 col-form-label text-md-right">{{ __('Current password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" required autofocus>
+
+                                @error('current_password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autofocus>
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" required>
+
+                                @error('password_confirmation')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Update password') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
